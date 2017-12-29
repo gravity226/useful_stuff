e2099265f58def800e8854958494cfcacf6ffd8b
Tommy Martin <tommymartin@Tommys-MacBook-Pro-2.local>
2017-12-29 11:43:23 -0700

    added iter_query to PGHelper

--- PGHelper/PGHelper.py
+++ PGHelper/PGHelper.py
@@ -1,9 +1,13 @@
 class PGHelper():
     import time
     import psycopg2
+    from psycopg2 import extras, extensions
     import os
 
-    def __init__(self, host=False, dbname=False, user=False, password=False, port=False):
+    def __init__(self, host=False, dbname=False, user=False, password=False, port=False, cursor_name=False):
+        # this will allow you to <control + c> during a long running query
+        self.extensions.set_wait_callback(self.extras.wait_select)
+
         try:
             self.host = host or self.os.environ['db_host']
             self.dbname = dbname or self.os.environ['db_name']
@@ -13,10 +17,17 @@ class PGHelper():
         except:
             raise Excption('No database credentials found')
 
+        self.cursor_name = cursor_name
+
         self.conn, self.cursor = self.connect_to_postgres()
 
     def __enter__(self):
         return self
+
+    def add_cursor_name(self, cursor_name):
+        self.cursor_name = cursor_name
+        self.conn.close()
+        self.conn, self.cursor = self.connect_to_postgres()
 
     def connect_to_postgres(self):
         for n in range(10):
@@ -24,11 +35,14 @@ class PGHelper():
                 conn_string = "host='%s' dbname='%s' user='%s' password='%s' port='%s'" % \
                     (self.host, self.dbname, self.user, self.password, self.port)
                 conn = self.psycopg2.connect(conn_string)
-                cursor = conn.cursor()
+                if self.cursor_name:
+                    cursor = conn.cursor(self.cursor_name)
+                else:
+                    cursor = conn.cursor()
                 return conn, cursor
             except:
                 self.time.sleep(5) # sleep for 5 seconds and try again
-                
+
         raise Exception ('ALERT!! Unable to connect to Postgres')
 
     def execute_query(self, query, has_results=True):
@@ -49,6 +63,14 @@ class PGHelper():
 
             return True
 
+    def iter_query(self, query):
+        if self.cursor_name:
+            self.cursor.execute(query)
+            for record in self.cursor:
+                yield record
+        else:
+            raise Exception('Must be using a named cursor. When you create the obj add the "cursor_name" parameter.')
+
     def rollback(self):
         self.conn.rollback()
 
@@ -57,16 +79,69 @@ class PGHelper():
 
 if __name__ == '__main__':
     import os
-
-    query = '''select * from schema.table limit 1;'''
-
+    from PGHelper import PGHelper
+    
     pg_helper = PGHelper(dbname=os.environ['db_name'],
                          host=os.environ['db_host'],
                          user=os.environ['db_user'],
                          password=os.environ['db_password'],
                          port=os.environ['db_port'])
 
-    results = pg_helper.execute_query(query=query)
+    results = pg_helper.execute_query(query='''select * from schema.table limit 1;''')
+    "select pg_sleep(20)"
+
+    # ------------------------
+    import os
+    from PGHelper import PGHelper
+
+    with PGHelper(dbname=os.environ['db_name'],
+                     host=os.environ['db_host'],
+                     user=os.environ['db_user'],
+                     password=os.environ['db_password'],
+                     port=os.environ['db_port']) as pg_helper:
+        results = pg_helper.execute_query(query='''select * from schema.table limit 1;''')
+
+    # ------------------------
+    from PGHelper import PGHelper
+    from guppy import hpy
+    h = hpy()
+    start = h.heap().size
+
+    with PGHelper(cursor_name='stream') as pg_helper:
+        results = pg_helper.execute_query(query='''select * from schema.table limit 10000;''')
+
+    finish = h.heap().size
+    print ''
+    print finish - start, 'bytes'
+    # >> 5,320,864 bytes
+
+    # ------------------------
+    from PGHelper import PGHelper
+    from guppy import hpy
+    h = hpy()
+    start = h.heap().size
+
+    with PGHelper(cursor_name='stream') as pg_helper:
+        query = '''select * from schema.table limit 10000;'''
+        for record in pg_helper.iter_query(query):
+            print record[0]
+    finish = h.heap().size
+    print ''
+    print finish - start, 'bytes'
+    # >> 8,000 bytes
+
+    # ------------------------
+    from PGHelper import PGHelper
+    from guppy import hpy
+    h = hpy()
+    start = h.heap().size
 
     with PGHelper() as pg_helper:
-        results = pg_helper.execute_query(query=query)
+        pg_helper.add_cursor_name(cursor_name='stream')
+        query = '''select * from schema.table limit 10000;'''
+        for record in pg_helper.iter_query(query=query):
+            print record[0]
+    finish = h.heap().size
+    print ''
+    print finish - start, 'bytes'
+    # >> 7,992 bytes
--- PGHelper/README.md
+++ PGHelper/README.md
@@ -1,6 +1,14 @@
 ## Postgres Helper
 This class is meant to make working with Postgres a little bit easier.
 
+## Table of Contents
+1. [What you need](https://github.com/gravity226/useful_stuff/tree/master/PGHelper#what-you-need)
+2. [Start using it](https://github.com/gravity226/useful_stuff/tree/master/PGHelper#start-using-it)
+3. [Setting up your ~/.bash_profile (Mac only) ](https://github.com/gravity226/useful_stuff/tree/master/PGHelper#optional---setting-up-your-bash_profile-mac-only)
+4. [Looking at Feature Importance](https://github.com/gravity226/useful_stuff/tree/master/NLP_Feature_Importance#looking-at-feature-importance)
+5. [Making New Predictions](https://github.com/gravity226/useful_stuff/tree/master/NLP_Feature_Importance#making-new-predictions)
+6. [Other Thoughts](https://github.com/gravity226/useful_stuff/tree/master/NLP_Feature_Importance#other-thoughts)
+
 #### What you need...
 1. Works with Python 2.7
 
@@ -74,3 +82,53 @@ print results
 ```
 
 When there are no values passed in, the class will attempt to grab them from the environment.
+
+## Don't load everything into memory
+- When you're working with larger datasets it's not a good idea to load all of your query results into memory.  What we can do instead is work with a named cursor.  We can do this by setting the parameter "cursor_name='anything'".  Then we can call the iter_query function and work with one record at a time.
+
+```Python
+from PGHelper import PGHelper
+
+# Don't forget to set up your environment variables
+with PGHelper(cursor_name='stream') as pg_helper:
+    query = '''select * from schema.table limit 10;'''
+    for record in pg_helper.iter_query(query):
+        print record[0]
+```
+
+- The point of this is to save on memory so let's test that.  In this example we will load all of the results into a variable called results and look at the memory usage.  If you don't already have guppy installed you can install it using "pip install guppy" in terminal.
+
+```Python
+from PGHelper import PGHelper
+from guppy import hpy
+h = hpy()
+start = h.heap().size
+
+with PGHelper(cursor_name='stream') as pg_helper:
+    results = pg_helper.execute_query(query='''select * from schema.table limit 10000;''')
+
+finish = h.heap().size
+print ''
+print finish - start, 'bytes'
+# >> 5320864 bytes
+```
+
+- Now let's use iter_query and see what the memory usage is
+
+```Python
+from PGHelper import PGHelper
+from guppy import hpy
+h = hpy()
+start = h.heap().size
+
+with PGHelper(cursor_name='stream') as pg_helper:
+    query = '''select * from schema.table limit 10000;'''
+    for record in pg_helper.iter_query(query):
+        print record[0]
+finish = h.heap().size
+print ''
+print finish - start, 'bytes'
+# >> 8,000 bytes
+```
+
+Looks like it's working.  5 million to 8 thousand bytes of memory is a pretty dramatic difference.
